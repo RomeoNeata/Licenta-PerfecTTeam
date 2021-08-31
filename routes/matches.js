a29d7b6e1646a9a73e018b56c01be23aa0b67c47
RomeoNeata <romeoneata@gmail.com>
2021-08-31 16:28:13 +0300

    commit

--- routes/matches.js
+++ routes/matches.js
@@ -1,5 +1,5 @@
 const express = require('express')
-const {getMatches, createMatch, matchesByUser, matchById, isPublisher, deleteMatch, updateMatch} = require("../controllers/match")
+const {getMatches, createMatch, matchesByUser, matchById, isPublisher, deleteMatch, updateMatch, userImage, getSingleMatch} = require("../controllers/match")
 const {requireSignin} = require("../controllers/auth")
 const {userById} = require("../controllers/user")
 const {createMatchValidator} = require('../validator/index')
@@ -14,12 +14,13 @@ router.post('/new/:userId',requireSignin, createMatch, createMatchValidator)
 
 //All matches created by a user
 router.get("/by/:userId", requireSignin, matchesByUser)
-router.delete('/:matchId', requireSignin, isPublisher, deleteMatch)
-router.put('/:matchId', requireSignin, isPublisher, updateMatch)
+router.get("match/:matchId", getSingleMatch)
+router.delete('match/:matchId', requireSignin, isPublisher, deleteMatch)
+router.put('match/:matchId', requireSignin, isPublisher, updateMatch)
 //any routes containing : userId, our app will first execute userByID()
 router.param("userId", userById)
 //any routes containing : matchId, our app will first execute matchByID()
-
+router.get("/matches/image/:matchId", userImage)
 router.param("matchId", matchById)
-
+router.param("userId", userById)
 module.exports = router
